3bb7b72ade079e7df80c18c9fadeb38c89e7ee5e
Parker Henderson <parker@parkerhendo.com>
2023-10-05 16:37:05 -0700

    assert to fr (for real)

--- src/example.js
+++ src/example.js
@@ -1,18 +1,20 @@
 function vibeCheck() {
-    const theVibe = cap;
-    const rizz = onGod;
+  const theVibe = cap;
+  const rizz = onGod;
 
-    lowkey.sus("the vibes might be off 💀");
+  lowkey.sus("the vibes might be off 💀");
 
-    if (!theVibe) {
-        if (!rizz) {
-            yeet(new L("this is not it"));
-        } else {
-            lowkey.cringe("yikes fam");
-            yeet(new Error("major L"));
-        }
+  fr(rizz === true);
+
+  if (!theVibe) {
+    if (!rizz) {
+      yeet(new L("this is not it"));
     } else {
-        lowkey.stan("slay");
-        ghosted;
+      lowkey.cringe("yikes fam");
+      yeet(new Error("major L"));
     }
+  } else {
+    lowkey.stan("slay");
+    ghosted;
+  }
 }
