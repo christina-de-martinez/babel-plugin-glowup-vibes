6a515e4ffe5ad2f15388a6267603ad23f47c2338
Kendrick Bong <kendrickbong1996@gmail.com>
2023-10-06 12:29:53 +0800

    hype

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
 module.exports = function () {
-    const identifierMappings = {
-        "onGod": "true",
-        "noCap": "true",
-        "cap": "false",
-        "ghosted": "return null",
-        "lowkey": "console",
-        "stan": "log",
-        "sus": "warn",
-        "cringe": "error",
-        "L": "Error"
-    };
+  const identifierMappings = {
+    onGod: 'true',
+    noCap: 'true',
+    cap: 'false',
+    ghosted: 'return null',
+    lowkey: 'console',
+    stan: 'log',
+    sus: 'warn',
+    cringe: 'error',
+    L: 'Error',
+    yeet: 'throw',
+    chill: 'setTimeout',
+    spill: 'return',
+    oof: 'console.error',
+    finesse: 'filter',
+    flex: 'console.assert',
+    clout: 'Math.max',
+    bop: 'Math.min',
+    lit: 'true',
+    slide: 'Array.push',
+    bounce: 'Array.pop',
+    squad: 'Array',
+    clique: 'Object',
+    hundoP: '100',
+    swag: 'JSON.stringify',
+    unswag: 'JSON.parse',
+    gucci: 'Promise.resolve',
+    busted: 'Promise.reject',
+    snack: 'fetch',
+    cash: 'parseInt',
+    float: 'parseFloat',
+    hype: 'addEventListener',
+    killHype: 'removeEventListener',
+  };
 
-    const handleIdentifier = (path) => {
-        const newName = identifierMappings[path.node.name];
-        if (newName) {
-            path.node.name = newName;
-        }
-    };
+  const handleIdentifier = (path) => {
+    const newName = identifierMappings[path.node.name];
+    if (newName) {
+      path.node.name = newName;
+    }
+  };
 
-    const handleExpressionStatement = (path) => {
-        const { node } = path;
-        
-        if (node.expression.type === "CallExpression" && node.expression.callee.name === "yeet") {
-            const errorArgument = node.expression.arguments[0];
-            const throwStatement = {
-                type: "ThrowStatement",
-                argument: errorArgument,
-            };
-            path.replaceWith(throwStatement);
-        }
-    };
+  const handleExpressionStatement = (path) => {
+    const {node} = path;
 
-    return {
-        visitor: {
-            Identifier: handleIdentifier,
-            ExpressionStatement: handleExpressionStatement
-        }
-    };
+    if (
+      node.expression.type === 'CallExpression' &&
+      node.expression.callee.name === 'yeet'
+    ) {
+      const errorArgument = node.expression.arguments[0];
+      const throwStatement = {
+        type: 'ThrowStatement',
+        argument: errorArgument,
+      };
+      path.replaceWith(throwStatement);
+    }
+  };
+
+  return {
+    visitor: {
+      Identifier: handleIdentifier,
+      ExpressionStatement: handleExpressionStatement,
+    },
+  };
 };
