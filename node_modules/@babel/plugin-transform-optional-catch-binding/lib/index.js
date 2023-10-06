aab9e959911057fde7158fcf77882cc8604c56d6
Nick K <nick@arguflow.gg>
2023-10-05 19:27:08 -0500

    add lib and node_modules to gitignore correctly

---
+++ node_modules/@babel/plugin-transform-optional-catch-binding/lib/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _helperPluginUtils = require("@babel/helper-plugin-utils");
+var _default = (0, _helperPluginUtils.declare)(api => {
+  api.assertVersion(7);
+  return {
+    name: "transform-optional-catch-binding",
+    inherits: require("@babel/plugin-syntax-optional-catch-binding").default,
+    visitor: {
+      CatchClause(path) {
+        if (!path.node.param) {
+          const uid = path.scope.generateUidIdentifier("unused");
+          const paramPath = path.get("param");
+          paramPath.replaceWith(uid);
+        }
+      }
+    }
+  };
+});
+exports.default = _default;
+
+//# sourceMappingURL=index.js.map
