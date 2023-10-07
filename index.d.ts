1938881c5517174d5fd58ec9d6e3e7857f8668d5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-06 19:41:38 -0700

    TypeScript Support
    Not sure how to handle `ghosted` and `return null` just yet.
    
    The TSConfig is only to demo these types in the source file, to show that it works at the type-checking level. To use the types in TS with this plugin, you just have to add `babel-plugin-glowup-vibes` to your own TSConfig in the `types` config option, and use Babel to transpile your TypeScript down to JS. Or another option is that you could use `// @ts-check` if you don't want to use the full TS syntax.

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+declare const onGod = true;
+declare const noCap = true;
+declare const cap = false;
+
+declare var lowkey: Console & {
+  stan: Console["log"];
+  sus: Console["warn"];
+  cringe: Console["error"];
+};
+
+declare var L: typeof Error;
+
+declare function yeet(error: any): never;
